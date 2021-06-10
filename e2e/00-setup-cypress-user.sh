c5e23738dd222be71cbf526c6df540b6219e0bdf
jbrown-xentity <jbrown@xentity.com>
2021-06-10 09:05:24 -0600

    Make test user in docker-compose.test

---
+++ e2e/00-setup-cypress-user.sh
@@ -0,0 +1,2 @@
+paster --plugin=ckan user add cypress-user password=cypress-user-password email=test@doi.gov -c /srv/app/production.ini
+paster --plugin=ckan sysadmin add cypress-user -c /srv/app/production.ini
