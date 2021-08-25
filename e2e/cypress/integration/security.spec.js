4940387b389d4cc96230e537d20a401951119ef4
jbrown-xentity <jbrown@xentity.com>
2021-08-25 12:03:45 -0600

    Add HSTS compliance test

--- e2e/cypress/integration/security.spec.js
+++ e2e/cypress/integration/security.spec.js
@@ -1,13 +1,4 @@
 describe('Security', () => {
-    // before(() => {
-    //     cy.login('cypress-user', 'cypress-user-password', false)
-    // })
-    // beforeEach(() => {
-    //     Cypress.Cookies.preserveOnce('auth_tkt', 'ckan')
-    // })
-    // after(() => {
-    //     cy.delete_organization('cypress-test-org')
-    // })
     it('Does not redirect to a given site', () => {
         cy.request({
             url: '//google.com/',
@@ -15,6 +6,15 @@ describe('Security', () => {
         }).should((response) => {
             cy.log(response)
             expect(response.status).to.eq(404)
+        })
+        
+    })
+
+    it('Is HSTS compliant', () => {
+        cy.request('/dataset').should((response) => {
+            expect(response).to.have.property('headers');
+            expect(response.headers).to.have.property('strict-transport-security');
+            expect(response.headers['strict-transport-security']).to.equal('max-age=31536000;');
         })
         
     })
