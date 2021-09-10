adb5cee82f53bd56d262af858aff6399ffa9b778
jbrown-xentity <jbrown@xentity.com>
2021-09-09 22:03:17 -0600

    Update to 15000 for buffer

--- ckan/create_datajson.sh
+++ ckan/create_datajson.sh
@@ -2,8 +2,8 @@
 
 
 sleep 10
-curl localhost:5001/internal/data.json --output /var/www/datajson/data.json --connect-timeout 10000 -m 10000
+curl localhost:5001/internal/data.json --output /var/www/datajson/data.json --connect-timeout 15000 -m 15000
 
 curl localhost:5001/api/action/organization_list | \
 jq -r '.result[]' | xargs -I'{}' \
-curl 'localhost:5001/organization/{}/data.json' --output '/var/www/datajson/{}-data.json' --connect-timeout 10000 -m 10000
+curl 'localhost:5001/organization/{}/data.json' --output '/var/www/datajson/{}-data.json' --connect-timeout 15000 -m 15000
