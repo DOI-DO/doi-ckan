770855768e82e1f72a6854d3d54d45b47c12c9ef
gil.platt <gplatt@xentity.com>
2021-10-15 14:20:33 -0600

    adding test for URLs and correct email

--- e2e/cypress/integration/default_email.spec.js
+++ e2e/cypress/integration/default_email.spec.js
@@ -2,56 +2,52 @@ describe('Distribution and ContactPoints are correct', () => {
     const dcatUsOrg = 'dcat-us-org';
     const harvestSource = 'dcat-waf-iso';
     
-    before(() => {
-        //cy.login('admin', 'password', false);
-        //cy.delete_organization(dcatUsOrg);
-        //cy.create_dcat_org(dcatUsOrg, 'description', false);
-        //cy.create_harvest_source();
-    });
-
     beforeEach(() => {
        Cypress.Cookies.preserveOnce('auth_tkt', 'ckan'); 
     });
 
-    after(() => {
-       //cy.delete_harvest_source(harvestSource);
-       //cy.delete_organization(dcatUsOrg);
-       //cy.logout();
-    });
-
     it('can validate default contact point fields and correct distribution fields', () => {
-        //rewrite for just organization creation at harvester level
-        //request data.json and check responses
-        
-        //cy.create_harvest_source('http://nginx-harvest-source/iso-waf/',
-            //harvestSource,
-            //'dcat waf iso',
-            //'waf',
-            //dcatUsOrg,
-            //false,
-            //false);
-       //cy.start_harvest_job(harvestSource)
+            let testDataset = '';
+            cy.writeFile('/home/gil/titles.txt', '');
+            cy.writeFile('/home/gil/urls.txt', '');
         cy.request('/data.json').then((response) => {
             expect(response.status).to.eq(200);
             const dcatUsObj = JSON.parse(response.body);
             const datasets = dcatUsObj['dataset'];
-            cy.writeFile('/home/gil/dcat.json', `${dcatUsObj}`)
             cy.log(dcatUsObj)
-            //harvest data
-            //start harvest job should wait to finish for request
+            let validUrls = [];
+            let invalidUrls = [];
             datasets.forEach((dataset) => {
                 cy.log(`${dataset['contactPoint']['hasEmail']}`)
-                cy.wrap(`${dataset['contactPoint']['hasEmail']}`).should('eq', /^mailto:[\\w\\_\\~\\!\\$\\&\\'\\(\\)\\*\\+\\,\\;\\=\\:.-]+@[\\w.-]+\\.[\\w.-]+?$/gm); 
+                if (dataset['title'] == 'EK500 Water Column Sonar Data Collected During AL0001') {
+                    testDataset = dataset;
+                } 
+                cy.writeFile('/home/gil/titles.txt', `${dataset['title']}\n`, {flag: 'a+'})
                 cy.log(`${dataset['contactPoint']['fn']}`);
                 const distribution = dataset['distribution'];
                 if (distribution) {
                     distribution.forEach((distSet) => {
                         let urlField = distSet['accessURL'] ? distSet['accessURL'] : distSet['downloadURL']
                         cy.log(`${urlField}`)
-                        
+                        cy.writeFile('/home/gil/urls.txt', `${urlField}\n`, {flag:'a+'})
+                        try {
+                            validUrls.push(new URL(urlField));
+                        } catch (_) {
+                            invalidUrls.push(urlField);
+                        }
                     });
                 }
             });
+            //there is a URL in validURLs, such that url='http://invalidURL'. This is considered valid, but was meant to
+            //be registered as invalid for testing
+            expect(invalidUrls).to.have.lengthOf(4);
+            expect(validUrls).to.have.lengthOf(16);
+       // dataset page should have an invalid email for a dataset. dcat-us should have default email that is valid for
+       // the same dataset
+       cy.visit('http://localhost:5000/dataset/ek500-water-column-sonar-data-collected-during-al0001');
+       cy.get('a[class="show-more"]').click();
+       cy.get('a[href="mailto:invalid"]').should('contain', 'Unknown');
+       cy.wrap(`${testDataset['contactPoint']['hasEmail']}`).should('eq', `mailto:jimmy_dean@${dcatUsOrg}.gov`);
        }); 
     });
 });
